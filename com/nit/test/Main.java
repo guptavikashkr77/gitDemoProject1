4c54269cd2f37af9c63cbad4ae42f4c8e4bae6fb
vikash kumar gupta <vikash kumar gupta@LAPTOP-PM40QTLP>
2021-06-05 09:19:36 +0530

    m2() method added

--- com/nit/test/Main.java
+++ com/nit/test/Main.java
@@ -6,6 +6,10 @@ public class Main {
 		System.out.println("Main.m1()");
 	}
 
+	public void m2() {
+		System.out.println("Main.m2()");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Inside the main method");
 	}
